036b3c22e8b52c954e8a78466b41b5fa8eafef45
mcle0341 <mcle0341@algonquinlive.com>
2016-04-02 19:56:39 -0400

    edited text

--- js/main.js
+++ js/main.js
@@ -1,39 +1,39 @@
-var intro = alert("Let's make a story together!");
+var popup = alert("Let's make a story together!");
 
-var colour = prompt("A colour (orange, indigo, etc.)");
+var colour = prompt('A colour (orange, indigo, etc.)');
 
-var creature = prompt("A creature (dragon, moth, etc.)");
+var creature = prompt('A creature (dragon, moth, etc.)');
 
-var adjective = prompt("A creature (dragon, moth, etc.)");
+var adjective = prompt('An adjective (beautiful, super, etc.)');
 
-var verb = prompt("choose a past tense verb (ran, kissed etc.)");
+var verb = prompt('A past tense verb (ran, kissed etc.)');
 
-var story = prompt("Which story would you like? Enter the letter below: a)snacking b)escaping c)winning");
+var story = prompt('Which story would you like? Enter the letter below: \na)snacking \nb)escaping \nc)winning');
 
 var storyA = function () {
 
-  document.write("After snacking on " + adjective + "treats, the" + colour + "bellied" + creature + " " + verb + "for hours.");
+  document.write('After snacking on ' + adj + ' treats, the ' + colour + ' bellied ' + creature + ' ' + past + ' for hours ');
 
 };
 
 var storyB = function () {
 
-  document.write("Ameilia " + verb + "through the " + adjective + " " + colour + "nebula escaping the space " + creature + ".");
+  document.write('Ameilia ' + past + ' through the ' + adj + ' ' + colour + ' nebula escaping the space ' + creature + '.');
 };
 
 var storyC = function () {
 
-  document.write("Jackson chose his" + adjective + ", " + colour + creature + "and " + verb + "it to the table knowing he won.");
+  document.write('Jackson chose his ' + adj + ' ' + colour + ' ' + creature + ' card and ' + past + ' it to the table knowing he won.');
 };
 
 alert("Let's make a story together!");
 
-if (story == "a" || story == "A") {
+if (story == 'a' || story == 'A') {
   storyA();
-} else if (story == "b" || story == "B") {
+} else if (story == 'b' || story == 'B') {
   storyB();
-} else if (story == "c" || story == "C") {
+} else if (story == 'c' || story == 'C') {
   storyC();
 } else {
-  document.write("You are no fun.");
+  document.write('You are no fun.');
 };
